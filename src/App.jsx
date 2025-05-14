612fbd392c32e1c5264027dc9824c040ec8f29e9
Jhonathan Velez <kokoa.arte@gmail.com>
2025-05-14 14:17:12 -0500

    firme commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './image/10elements.jpeg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+          <img src={reactLogo} className="logo" alt="Vite logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
